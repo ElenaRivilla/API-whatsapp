8330a955de5f4369d59a981a4ee5988687a9fceb
ivan <ivanmartin@paucasesnovescifp.cat>
2025-01-24 21:02:24 +0100

    Cambiado el contenido del error

--- messaging-app/frontend/js/errContol.js
+++ messaging-app/frontend/js/errContol.js
@@ -3,11 +3,11 @@ export function validateLogin(username, password) { // TODO Revisar como exporta
     const regex = /(^[0-9]{7}[A-Z]$)|(^[0-9]{8}$)/;
     const regexUser = /^[^0-9]*$/;
     if (!regexUser.test(username)) {
-        throw new Error("Username must not contain numbers.");
+        throw new Error("El nombre de usuario no puede contener números.");
     }
 
     if (!regex.test(password)) {
-        throw new Error("Password must be a NIE with 7 digits + 1 uppercase letter or DNI with 8 digits.");
+        throw new Error("La contraseña en NIE necesita 7 dígitos + 1 letra en mayúsculas o el DNI con 8 dígitos.");
     }
     return;
 }
