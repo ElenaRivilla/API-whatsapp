b503970861d63544a55b314617dc6541a4b49b0b
Facundo-Nastasi-DAW <facundonastasi@paucasesnovescifp.cat>
2025-01-27 18:00:11 +0100

    endpoint "check"

--- messaging-app/api/app.py
+++ messaging-app/api/app.py
@@ -5,21 +5,29 @@ from database import database
 db = database()
 app = FastAPI()
 
-#Get group messages
+#End-point to get group messages
 @app.get('/getMessages/{loadSize}/{idGroup}')
 def getGroupMessages(loadSize: int, idGroup: int):
     messages = db.getMessages(loadSize, idGroup)
     return messages
 
-#Get specific chat messages
+#End-point to get chat messages between two users
 @app.get('/getMessages/{loadSize}/{user1}/{user2}')
 def getUsersMessages(loadSize: int, user1: str , user2: str):
-    id1 = db.getUserId(user1)
-    id2 = db.getUserId(user2)
-    messages = db.getMessages(loadSize, id1, id2)
+    messages = db.getMessages(loadSize, db.getUserId(user1), db.getUserId(user2))
     return messages
-# Function to get specific user friends
+
+# End-point to get user friends
 @app.get('/getFriends/{username}')
 def getFriends(username: str):
-    friendsList = db.getFriends(username)
+    friendsList = db.getFriends(db.getUserId(username))
     return friendsList
+
+# End-point to change message status
+@app.get('/check/{messageId}')
+def check(messageId: int):
+    try:
+        db.checkMessage(messageId)
+        return
+    except Exception as e:
+        raise e
