49ad371dcc8640da5939c34e76cd34c550cf96be
Elena Rivilla <mariaelenarivilla@paucasesnovescifp.cat>
2025-01-27 17:36:48 +0100

    Refactor getUsersMessages to retrieve user IDs before fetching messages

--- messaging-app/api/app.py
+++ messaging-app/api/app.py
@@ -13,6 +13,8 @@ def getGroupMessages(loadSize, idGroup):
 
 #Get specific chat messages
 @app.get('/getMessages/{loadSize}/{user1}/{user2}')
-def getUsersMessages(loadSize, user1, user2):
-    messages = db.getMessages(loadSize, user1, user2)
+def getUsersMessages(loadSize: int, user1: str , user2: str):
+    id1 = db.getUserId(user1)
+    id2 = db.getUserId(user2)
+    messages = db.getMessages(loadSize, id1, id2)
     return messages
