6502fd8104de8d7ca5f80f538cd565f85b5f5df4
Facundo-Nastasi-DAW <facundonastasi@paucasesnovescifp.cat>
2025-01-27 17:25:03 +0100

    crud acabado

--- messaging-app/api/database.py
+++ messaging-app/api/database.py
@@ -45,9 +45,17 @@ class database(object):
         self.desconecta()
         return ResQuery
     
-    def userExists(self, username):
+    def getUserId(self, username):
         self.conecta()
-        sql=f'SELECT * from usuarisclase where username = "{username}";'
+        sql=f'SELECT id from usuarisclase where username = "{username}";'
+        self.cursor.execute(sql)
+        ResQuery=self.cursor.fetchone()
+        self.desconecta()
+        return ResQuery['id']
+
+    def userExists(self, userId):
+        self.conecta()
+        sql=f'SELECT * from usuarisclase where id = "{userId}";'
         self.cursor.execute(sql)
         ResQuery=self.cursor.fetchone()
         self.desconecta()
@@ -55,11 +63,11 @@ class database(object):
             return True
         return False
     
-    def deleteUser(self, username):
-        if not self.userExists(username):
+    def deleteUser(self, userId):
+        if not self.userExists(userId):
             return {"status": "Non-existant user"}
         self.conecta()
-        sql=f'DELETE from usuarisclase where username = "{username}";'
+        sql=f'DELETE from usuarisclase where id = "{userId}";'
         self.cursor.execute(sql)
         self.desconecta()
         return {"status": "Succesfully deleted"}
@@ -79,6 +87,44 @@ class database(object):
             return {"status": "Non-existant group"}
         self.conecta()
         sql=f'DELETE from groups where id = {groupId};'
+        self.cursor.execute(sql)
+        self.desconecta()
+        return {"status": "Succesfully deleted"}
+    
+    def messageExists(self, messageId):
+        self.conecta()
+        sql=f'SELECT * from message where id = {messageId};'
+        self.cursor.execute(sql)
+        ResQuery=self.cursor.fetchone()
+        self.desconecta()
+        if ResQuery:
+            return True
+        return False
+    
+    def deleteMessage(self, messageId):
+        if not self.userExists(messageId):
+            return {"status": "Non-existant message"}
+        self.conecta()
+        sql=f'DELETE from message where id = {messageId};'
+        self.cursor.execute(sql)
+        self.desconecta()
+        return {"status": "Succesfully deleted"}
+    
+    def userExistsInGroup(self, userId, groupId):
+        self.conecta()
+        sql=f'SELECT * from user_group where id_user = {userId} and id_group = {groupId};'
+        self.cursor.execute(sql)
+        ResQuery=self.cursor.fetchone()
+        self.desconecta()
+        if ResQuery:
+            return True
+        return False
+    
+    def deleteUserFromGroup(self, userId, groupId):
+        if not self.userExistsInGroup(userId, groupId):
+            return {"status": "User not registered in group"}
+        self.conecta()
+        sql=f'DELETE from user_group where where id_user = {userId} and id_group = {groupId};'
         self.cursor.execute(sql)
         self.desconecta()
         return {"status": "Succesfully deleted"}
