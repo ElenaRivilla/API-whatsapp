4e8182cd9d5879108be25ede93d50832900083cc
Elena Rivilla <mariaelenarivilla@paucasesnovescifp.cat>
2025-01-31 18:56:55 +0100

    Añadido objetos para hacer tests con los contactos y el chat

--- messaging-app/frontend/tests/tests.js
+++ messaging-app/frontend/tests/tests.js
@@ -1,4 +1,4 @@
-import * as errControl from "../js/errContol";
+//import * as errControl from "../js/errControl.js";
 
 function testLoginValid() {
     const testData = [
@@ -37,4 +37,89 @@ function testErrControl(){
     testLoginValid();
 }
 
-testErrControl();
+/* testErrControl(); */
+
+//chats de pruebas
+export const openChats = [
+    {
+        username: "Alice Johnson",
+        message: "Mensaje de Alice que debería truncarse con puntos suspensivos si es demasiado largo para el contenedor.",
+        time: "10:00h",
+        imageUrl: "https://picsum.photos/300/300?random=7"
+    },
+    {
+        username: "Bob Smith",
+        message: "Este es un mensaje de Bob que también debería truncarse si es demasiado largo para el contenedor.",
+        time: "10:30h",
+        imageUrl: "https://picsum.photos/300/300?random=8"
+    },
+    {
+        username: "Charlie Brown",
+        message: "Mensaje de Charlie que debería truncarse con puntos suspensivos si es demasiado largo para el contenedor.",
+        time: "11:00h",
+        imageUrl: "https://picsum.photos/300/300?random=9"
+    },
+    {
+        username: "Diana Prince",
+        message: "Este es un mensaje de Diana que también debería truncarse si es demasiado largo para el contenedor.",
+        time: "11:30h",
+        imageUrl: "https://picsum.photos/300/300?random=10"
+    },
+    {
+        username: "Eve Adams",
+        message: "Mensaje de Eve que debería truncarse con puntos suspensivos si es demasiado largo para el contenedor.",
+        time: "12:00h",
+        imageUrl: "https://picsum.photos/300/300?random=11"
+    }
+];
+
+export const messages = [
+    {
+        type: "sender",
+        imageUrl: "https://picsum.photos/300/300?random=1",
+        message: "Hola, ¿cómo estás?",
+        time: "10:35"
+    },
+    {
+        type: "receiver",
+        message: "¡Hola! Estoy bien, ¿y tú?",
+        time: "10:36",
+        checkImageUrl: "../assets/svg/double-check-blue.svg"
+    },
+    {
+        type: "sender",
+        imageUrl: "https://picsum.photos/300/300?random=1",
+        message: "También estoy bien, gracias. ¿Qué planes tienes para hoy?",
+        time: "10:37"
+    },
+    {
+        type: "receiver",
+        message: "Voy a trabajar en un proyecto. ¿Y tú?",
+        time: "10:38",
+        checkImageUrl: "../assets/svg/double-check-blue.svg"
+    },
+    {
+        type: "sender",
+        imageUrl: "https://picsum.photos/300/300?random=1",
+        message: "Tengo una reunión en la tarde, pero después estoy libre. Podemos vernos y discutir sobre el proyecto que mencionaste la última vez. Me parece una gran oportunidad para avanzar y resolver algunas dudas que tengo. Además, podríamos aprovechar para revisar los documentos que nos enviaron y planificar las siguientes etapas. ¿Qué te parece? Si tienes alguna otra idea o sugerencia, estaré encantado de escucharla. Nos vemos más tarde.",
+        time: "10:39"
+    },
+    {
+        type: "receiver",
+        message: "Genial, tal vez podamos vernos después.",
+        time: "10:40",
+        checkImageUrl: "../assets/svg/double-check-blue.svg"
+    },
+    {
+        type: "sender",
+        imageUrl: "https://picsum.photos/300/300?random=1",
+        message: "Gracias, ¡Hasta luego!",
+        time: "10:39"
+    },
+    {
+        type: "receiver",
+        message: "¡Hasta luego!",
+        time: "10:40",
+        checkImageUrl: "../assets/svg/double-check-blue.svg"
+    }
+];
