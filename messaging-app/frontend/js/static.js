a7fbe31883d1b6e4acf2a91eca007aa74909f998
Ivan Martin <159585007+Ivanoviix@users.noreply.github.com>
2025-02-09 21:15:17 +0100

    cambiado nombre de variable

--- messaging-app/frontend/js/static.js
+++ messaging-app/frontend/js/static.js
@@ -15,16 +15,16 @@ function generateRightPanelFund() {
     messagesContainer.append(imageSVG, doveContainer, textContainer);
 
 
-    const footer = $("<div>").addClass("footer back-bar h-4 mb-3 flex items-center justify-center").css({
+    const footerContiner = $("<div>").addClass("footer back-bar h-4 mb-3 flex items-center justify-center").css({
         "position": "absolute",
         "bottom": "0",
         "width": "100%",
         "z-index": "1000"
     });
 
-    chatContainer.append(footer);
+    chatContainer.append(footerContiner);
 
     const footerText = $("<p>").addClass("flex items-center justify-center h-full text-base mr-2 italic").text("Creado por la empresa TriCoded");
     const footerSVG = $("<img>").addClass("h-8").attr("src", "../assets/svg/logoCompany.svg");
-    footer.append(footerText, footerSVG);
+    footerContiner.append(footerText, footerSVG);
 }
