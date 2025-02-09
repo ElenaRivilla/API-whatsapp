848ce2ff9918a45995a8f36497f57668989cf2d6
Ivan Martin <159585007+Ivanoviix@users.noreply.github.com>
2025-02-09 21:54:58 +0100

    refactor: arreglado fallo del panel derecho

--- messaging-app/frontend/js/static.js
+++ messaging-app/frontend/js/static.js
@@ -6,23 +6,17 @@ function cargaDOM() {
 
 function generateRightPanelFund() {
 
-    const chatContainer = $(".chats").css("position", "relative");
+    const messagesContainer = $(".messages-container");
 
-    const messagesContainer = $(".messages-container").addClass("flex flex-col items-center justify-center")
+    const groupTextContainer = $("<div>").addClass("group-text-container flex flex-col items-center justify-center h-[95%]");
     const imageSVG = $("<img>").addClass("h-28 mb-4").attr("src", "../assets/svg/dove.svg");
     const doveContainer = $("<h2>").addClass("mb-2 font-bold").text("Dove");
     const textContainer = $("<h2>").addClass("font-bold").text("Conversa sin límites, conecta con el mundo.");
-    messagesContainer.append(imageSVG, doveContainer, textContainer);
+    groupTextContainer.append(imageSVG, doveContainer, textContainer);
 
+    const footerContiner = $("<div>").addClass("footer back-bar h-4 mb-3 flex items-center justify-center bottom-0");
 
-    const footerContiner = $("<div>").addClass("footer back-bar h-4 mb-3 flex items-center justify-center").css({
-        "position": "absolute",
-        "bottom": "0",
-        "width": "100%",
-        "z-index": "1000"
-    });
-
-    chatContainer.append(footerContiner);
+    messagesContainer.append(groupTextContainer, footerContiner);
 
     const footerText = $("<p>").addClass("flex items-center justify-center h-full text-base mr-2 italic").text("Creado por la empresa TriCoded");
     const footerSVG = $("<img>").addClass("h-8").attr("src", "../assets/svg/logoCompany.svg");
