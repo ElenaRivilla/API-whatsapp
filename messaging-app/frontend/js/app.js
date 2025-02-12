d48d15aab947a473fbd41c328f03ada0e66ce791
ivan <ivanmartin@paucasesnovescifp.cat>
2025-02-12 19:30:56 +0100

    feat: add group creation function to handle group setup

--- messaging-app/frontend/js/app.js
+++ messaging-app/frontend/js/app.js
@@ -243,6 +243,10 @@ function home() {
         searchBar.addClass("block");
     }
 
+    function creationGroup(name, description, userList, admin) {
+        const response = createGroup(name, description, userList, admin)
+    }
+
     // setInterval(loadFriends(), 30000); // que lo haga cada x minutos, asi se refrescan los mensajes
     loadFriends();
     // hacer add event-listeners a los botones como mostrar chat, nuevo grupo y settings, para que cambien el dom
