18e68a6b2877709aa9aae81d403ccaa53284a2e9
Facundo-Nastasi-DAW <facundonastasi@paucasesnovescifp.cat>
2025-02-21 00:15:56 +0100

    obviar este commit

--- messaging-app/frontend/js/static.js
+++ messaging-app/frontend/js/static.js
@@ -98,14 +98,3 @@ export function setLightMode() {
     textBar = rootStyles.getPropertyValue('--typebar-color').trim();
     sendButton = rootStyles.getPropertyValue('--send-button-color').trim();
 }
-
-export function errorMessage() {
-    const containerError = $('<div>').addClass('display: flex; position: fixed; justify-content: space-between; background-color: #f3f4f6e1; z-index: 999; top: 0; left: 0; width: 100%; height: 6rem; padding-left: 5%; padding-right: 5%;');
-    const textError = $('<p>').addClass('text-red-500 flex flex-col justify-center items-center font-bold ');
-    const buttonError = $('<button>').addClass('ml-2').append($('<img>').attr('src', '../assets/svg/close.svg').css({ width: '2rem', marginLeft: '2rem' })
-    );
-
-    containerError.append(textError, buttonError);
-    $('body').prepend(containerError); // Add the error message container to the top of the body
-    buttonError.on('click', () => containerError.remove());
-}
